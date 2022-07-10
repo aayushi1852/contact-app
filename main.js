af754cd9e763cbb8566305218aec933a1b4e569f
aayushi <aayushishah2972@gmail.com>
2022-07-10 01:06:01 -0400

    create form

---
+++ main.js
@@ -0,0 +1,13 @@
+
+let config = {
+    apiKey: "AIzaSyBRoSAmXZEZvZueAhy-kqzm4E-iNyyvy6c",
+    authDomain: "contacts-a25d3.firebaseapp.com",
+    databaseURL: "https://contacts-a25d3-default-rtdb.firebaseio.com",
+    projectId: "contacts-a25d3",
+    storageBucket: "contacts-a25d3.appspot.com",
+    messagingSenderId: "726215056953",
+    appId: "1:726215056953:web:1f23967bee1450506e77e8"
+  };
+  firebase.initializeApp(config);
+  let db = firebase.database();
+  
