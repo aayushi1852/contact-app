9acf7eb0d5bd0cd92c5e454e190394510c44a889
aayushi <aayushishah2972@gmail.com>
2022-07-11 17:21:27 -0400

    create view contacts page and style home and add contact page

--- js/view.js
+++ js/view.js
@@ -16,3 +16,23 @@ var Contact = document.getElementById('contact');
 var contactForm = document.getElementById('contactForm');
 var hiddenId = document.getElementById('hiddenId');
 
+var reviews = document.getElementById('reviews');
+var reviewsRef = db.ref('/contacts');
+
+let a=1;
+reviewsRef.on('child_added', (data) => {
+  var li = document.createElement('tr')
+  li.id = data.key;
+  li.innerHTML = reviewTemplate(data.val(),a);
+  reviews.appendChild(li);
+  a++;
+});
+
+function reviewTemplate(data,a) {
+  return `
+    <td>${a}</td>
+    <td>${data.name}</td>
+    <td>${data.email}</td>
+    <td>${data.contact}</td>
+  `
+};
